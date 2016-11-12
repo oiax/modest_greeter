d195e1e2664ce0e2e8eff7a7f99f614025851dd1
Tsutomu Kuroda <t-kuroda@oiax.jp>
2016-11-12 12:58:41 +0900

    Simplify TopView.document_title func

--- web/views/top_view.ex
+++ web/views/top_view.ex
@@ -2,8 +2,8 @@ defmodule ModestGreeter.TopView do
   use ModestGreeter.Web, :view
 
   def document_title(assigns) do
-    case assigns do
-      %{ view_template: "about.html" } ->
+    case assigns.view_template do
+      "about.html" ->
         "ModestGreeter - このサイトについて"
       _ ->
         "ModestGreeter"
