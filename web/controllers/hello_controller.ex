bb0d80e1c091a87ea88619a6745ddeaa1dcea4d6
Tsutomu Kuroda <t-kuroda@oiax.jp>
2017-11-14 20:40:22 +0900

    [volume01] ch08

--- web/controllers/hello_controller.ex
+++ web/controllers/hello_controller.ex
@@ -2,6 +2,6 @@ defmodule ModestGreeter.HelloController do
   use ModestGreeter.Web, :controller
 
   def show(conn, _params) do
-    render conn, "show.html"
+    render conn, "show.html", name: "Alice"
   end
 end
