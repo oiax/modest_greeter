8c4528c2cbdb7e965527cc1ac14ddb99a2ccec72
Tsutomu Kuroda <t-kuroda@oiax.jp>
2020-01-02 20:38:44 +0900

    [v01/ch08] params to templates

--- lib/modest_greeter_web/controllers/hello_controller.ex
+++ lib/modest_greeter_web/controllers/hello_controller.ex
@@ -1,7 +1,7 @@
-defmodule ModestGreeterWeb.HelloController do 
+defmodule ModestGreeterWeb.HelloController do
   use ModestGreeterWeb, :controller
 
   def show(conn, _params) do
-    render conn, "show.html"
+    render conn, "show.html", name: "Alice"
   end
 end
